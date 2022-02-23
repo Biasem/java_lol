3e3150d0f34f3c7545d7bbdb41250bbe15f59cfa
Ruben <rnieto@safareyes.es>
2022-02-23 21:59:37 +0100

    jdk11 v2

---
+++ src/main/java/utilidades/UtilidadesFicheros.java
@@ -0,0 +1,62 @@
+package utilidades;
+
+import com.opencsv.CSVReader;
+import modelos.Personaje;
+import modelos.Region;
+
+
+import java.awt.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UtilidadesFicheros {
+
+
+    public UtilidadesFicheros() {
+    }
+
+    public static final char SEPARATOR=';';
+    public static final char QUOTE='"';
+
+    public static List<Personaje>  leerFicheroPersonajes() throws IOException {
+
+        List<Personaje> personajes = new ArrayList<>();
+        CSVReader reader = null;                          //"D:\\Descargas Utorrent\\estudios\\DAW 1\\java proyect lol\\src\\main\\java\\coche.csv"
+        try {                                            //"E:\\java proyect 2\\src\\main\\java\\Coche.csv"
+            reader = new CSVReader(new FileReader("D:\\Descargas Utorrent\\estudios\\DAW 1\\java proyect lol\\src\\main\\java\\coche.csv"),SEPARATOR,QUOTE);
+            String[] nextLine= null ;
+            int count = 0;
+
+            while ((nextLine = reader.readNext()) != null) {
+
+                if(count >0) {
+                    String[] valores = nextLine[0].toString().split(",");
+                    Personaje p = new Personaje();
+                    p.setId(Integer.parseInt(valores[0]));
+                    p.setNombre(valores[1]);
+                    p.setFechaCreacion(LocalDate.parse(valores[2], DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+                    p.setAtaqueBase( Double.parseDouble(valores[3]));
+                    p.setDefensaBase( Double.parseDouble(valores[4]));
+                    p.setVidaBase( Double.parseDouble(valores[5]));
+                    p.setManaBase( Double.parseDouble(valores[6]));
+                    p.setRegion( Region.valueOf(valores[7]));
+                    personajes.add(p);
+                }
+                count++;
+            }
+
+        } catch (Exception e) {
+            throw e;
+        }
+
+        reader.close();
+
+        return  personajes;
+    }
+
+}
---
+++ src/main/java/utilidades/UtilidadesPersonaje.java
@@ -0,0 +1,74 @@
+package utilidades;
+
+import modelos.Escalabilidad;
+import modelos.Personaje;
+import modelos.Region;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UtilidadesPersonaje {
+
+    //Que recibiendo un personaje aumente su nivel en 1, actualizando
+    //todas sus estadísticas según su escalabilidad.
+    public Personaje levelUp(Personaje personaje){
+        if (personaje.getNivel()<18) {
+            personaje.setNivel(personaje.getNivel() + 1);
+            personaje.setAtaqueBase(personaje.getAtaqueBase() + personaje.getEscalabilidad().getIncrementoDanioNivel() * personaje.getNivel());
+            personaje.setDefensaBase(personaje.getDefensaBase() + personaje.getEscalabilidad().getIncrementoDefensaNivel() * personaje.getNivel());
+            personaje.setManaBase(personaje.getManaBase() + personaje.getEscalabilidad().getIncrementoManaNivel() * personaje.getNivel());
+            personaje.setVidaBase(personaje.getVidaBase() + personaje.getEscalabilidad().getIncrementoSaludNivel() * personaje.getNivel());
+        }
+        return personaje;
+    }
+
+    //Que a partir de un listado de personajes devuelve un mapa de los
+    //personajes agrupados por región.
+    public Map<Region,List<Personaje>> getPersonajesPorRegion(List<Personaje> personajes){
+
+        return personajes.stream().collect(Collectors.groupingBy(p-> p.getRegion()));
+    }
+
+    //A partir de una lista de personajes, devuelve el personaje más
+    //poderoso.
+    public Personaje getMasPoderoso(List<Personaje> personaje){
+        Personaje poderoso = new Personaje();
+        for (Personaje perso :personaje){
+            while(perso.getNivel()<18){
+                levelUp(perso);
+            }
+            if (poderoso.getNivel()==null){
+                poderoso = perso;
+            }
+            if((perso.getAtaque()+ perso.getDefensa()+perso.getMana()+perso.getVida())>
+                    (perso.getAtaque())+poderoso.getDefensa()+ poderoso.getMana()+ poderoso.getVida()){
+                poderoso = perso;
+            }
+        }
+        return poderoso;
+    }
+
+    //Que devuelve el personaje más poderoso de cada región.
+
+    public Map<Region, Personaje> getMasPoderosoPorRegion(List<Personaje> personajes){
+        Map<Region,Personaje> mapa = new HashMap<>();
+
+        mapa.put(Region.DEMACIA,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.DEMACIA)).collect(Collectors.toList())));
+        mapa.put(Region.NOXUS,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.NOXUS)).collect(Collectors.toList())));
+        mapa.put(Region.JONIA,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.JONIA)).collect(Collectors.toList())));
+        mapa.put(Region.SHURIMA,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.SHURIMA)).collect(Collectors.toList())));
+        mapa.put(Region.TARGON,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.TARGON)).collect(Collectors.toList())));
+        mapa.put(Region.FREIJORD,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.FREIJORD)).collect(Collectors.toList())));
+        mapa.put(Region.PILTOVER,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.PILTOVER)).collect(Collectors.toList())));
+        mapa.put(Region.ZAUM,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.ZAUM)).collect(Collectors.toList())));
+        mapa.put(Region.BANDLE,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.BANDLE)).collect(Collectors.toList())));
+        mapa.put(Region.AGUAS_ESTANCADAS,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.AGUAS_ESTANCADAS)).collect(Collectors.toList())));
+        mapa.put(Region.DESCONOCIDA,getMasPoderoso(personajes.stream().filter(p-> p.getRegion().equals(Region.DESCONOCIDA)).collect(Collectors.toList())));
+
+        return mapa;
+    }
+
+
+}
