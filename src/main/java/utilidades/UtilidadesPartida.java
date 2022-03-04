d791c5c59426872186e1ea59f3727d40beb429e8
PC38\daw20 <rnietourena@safareyes.es>
2022-03-04 20:29:55 +0100

    v7

--- src/main/java/utilidades/UtilidadesPartida.java
+++ src/main/java/utilidades/UtilidadesPartida.java
@@ -4,13 +4,31 @@ import modelos.Jugador;
 import modelos.Partida;
 import modelos.Personaje;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class UtilidadesPartida implements IUtilidadesPartida{
 
     public void inicializarPartida(Partida partida, List<Jugador> participantes, List<Personaje> personajesDisponibles){
+       Map<Jugador,Personaje> mapaJugadorPersonaje = new HashMap<>();
+       Map<Integer, Set<Jugador>> mapaEquipos = new HashMap<>();
+       for (Jugador j: participantes){
+           List<Personaje> eleccion = new ArrayList<>(personajesDisponibles);
+           eleccion.retainAll(j.getPersonajesFavoritos());
+           if (!eleccion.isEmpty()){
+               int rnd = (int) (Math.random()*( eleccion.size()-1));
+               mapaJugadorPersonaje.put(j, eleccion.get(rnd));
+           }else{
+               int rnd = (int) (Math.random()*( personajesDisponibles.size()-1));
+               mapaJugadorPersonaje.put(j, eleccion.get(rnd));
+           }
+       }
 
 
+
+       partida.setElecciones(mapaJugadorPersonaje);
+       partida.setJugadoresPorEquipo(mapaEquipos);
+       partida.setInicioPartida(LocalDateTime.now());
     }
 
     public void finalizarPartida(Partida partida, Integer equipoVencedor){
