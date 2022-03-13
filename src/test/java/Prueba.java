408598f7d56a997ef6f8ce564ada7eb556f93d14
Ruben <rnieto@safareyes.es>
2022-03-13 15:10:11 +0100

    probar metodos

--- src/test/java/Prueba.java
+++ src/test/java/Prueba.java
@@ -1,3 +1,5 @@
+import modelos.Jugador;
+import modelos.Personaje;
 import utilidades.UtilidadesFicheros;
 
 import java.io.IOException;
@@ -7,16 +9,11 @@ import java.util.*;
 public class Prueba {
 
     public static void main(String[] args) throws IOException {
-       // System.out.println(UtilidadesFicheros.leerItems());
-        /*int tiempoinicio;
-        tiempoinicio = LocalDateTime.now().getSecond();
-        try {
-            Thread.sleep(3000);
-        } catch(InterruptedException e) {
-            System.out.println("programa en espera");
-        }
-        int tiempofin = LocalDateTime.now().getSecond();
-        System.out.println(tiempofin-tiempoinicio);*/
+
+        List<Personaje> p1 = UtilidadesFicheros.leerPersonajes();
+
+        Jugador j1 = new Jugador(1,"xanos",);
+
 
 
 
